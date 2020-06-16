1530b696f09f603211b0a67f3e4e220bf2433d25
Denzell Ford <fdenzell@gmail.com>
2020-06-16 12:47:44 -0500

    added Class database between Student and Teacher

--- src/user/models.py
+++ src/user/models.py
@@ -1,27 +1,43 @@
 from src.ext import db
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.schema import Table
 from sqlalchemy.orm import relationship
 
 
 class Teacher(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
-    username = db.Column(db.String, unique=True)
-    slug = db.Column(db.String)
-    email = db.Column(db.String)
-    passowrd = db.Column(db.String(256))
+    username = Column(String, unique=True)
+    slug = Column(String, index= True)
+    email = Column(String)
+    passowrd = Column(String(256))
 
     students = relationship("Student", backref = "teacher" )
 
+# maps students to classes
+student_class = db.Table('student_class',
+        Column('student_id', Integer, db.ForeignKey('student.id')),
+        Column('class_id', Integer, db.ForeignKey('class.id')),
+)
+
+class Class(db.Model):
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String)
+    joinCode = Column(String(8))
+
 class Student(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
-    username = db.Column(db.String, unique=True)
-    slug = db.Column(db.String)
-    email = db.Column(db.String)
-    passowrd = db.Column(db.String(256))
+    username = Column(String, unique=True)
+    slug = Column(String, index= True)
+    email = Column(String)
+    passowrd = Column(String(256))
 
-    teacher_id = db.Column(db.Integer, db.ForeignKey("teacher.id"))
+    classes = relationship('Class', secondary=student_class, backref=db.backref('students', lazy='dynamic'))
 
+    teacher_id = Column(Integer, db.ForeignKey("teacher.id"))
 
